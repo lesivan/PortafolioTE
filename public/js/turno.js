8ce371fd38bd05f3db5eeba2f30cbe0e515a6621
Lester <lestermt94@gmail.com>
2016-10-26 22:32:03 -0600

    Turno JS

---
+++ public/js/turno.js
@@ -0,0 +1,85 @@
+$(function(){
+	$(".select2").select2();
+	listar();
+
+});
+
+
+function listar(){
+	$('#datos').empty();
+	$.get('/turnos', function(res){
+		$(res).each(function(key, value){
+			$('#datos').append('<tr><td>'+(key+1)+'</td>'+
+				'<td>'+value.descripcion+'</td>'+
+				'<td><button value='+value.idturno+' OnClick="mostrar(this);" class="btn btn-primary" data-toggle="modal" data-target="#modalEdit">Editar</button> '+
+				'<button value='+value.idturno+' OnClick="danger(this);" class="btn btn-danger" data-toggle="modal" data-target="#modalRemove">Eliminar</button>'+'</td></tr>');
+		});
+	});
+}
+
+
+
+$('#agregar').on('click', function(){
+	
+	var JData = {
+		descripcion: $('#turno').val()
+		};
+	$.ajax({
+		url: '/turno',
+		headers: {'X-CSRF-TOKEN': $('#token').val()},
+		type: 'POST',
+		dataType: 'json',
+		data: JData,
+
+		success: function(){
+			listar();
+			$('#turno').val('');
+			
+		
+
+			$('#msjuser').removeClass('alert-danger');
+			$('#msjuser').addClass('alert-success');
+			$('#msjuser'+'-text').html('Registro agregado exitosamente!');
+			$('#msjuser').fadeIn();
+			window.setTimeout(function(){$('#msjuser').fadeOut();}, 2000);
+		},
+		error:function(msj){
+			$('#msjuser').removeClass('alert-success');
+			$('#msjuser').addClass('alert-danger');
+			$('#msjuser'+'-text').html((msj.responseJSON.descripcion!=undefined)?msj.responseJSON.descripcion:'');
+			$('#msjuser').fadeIn();
+			window.setTimeout(function(){$('#msjuser').fadeOut();}, 2000);
+		}
+	});
+});
+
+
+
+
+function danger(btn){
+	$('#confirmRemove').val(btn.value);
+}
+
+function eliminar(btn){
+	$.ajax({
+		url: '/turno/'+btn.value,
+		headers: {'X-CSRF-TOKEN': $('#token').val()},
+		type: 'DELETE',
+		dataType: 'json',
+		success: function(){
+			listar();
+			$('#modalRemove').modal('toggle');
+			$('#msjuser').removeClass('alert-danger');
+			$('#msjuser').addClass('alert-success');
+			$('#msjuser'+'-text').html('Registros actualizados exitosamente!');
+			$('#msjuser').fadeIn();
+			window.setTimeout(function(){$('#msjuser').fadeOut();}, 2000);
+		}
+	});
+}
+
+function mostrar(btn){
+	$.get('/turno/'+btn.value+'/edit', function(res){
+		$('#descripcion').val(res.NombreCarrera);
+	})	
+}
