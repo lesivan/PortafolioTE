3724855266e409a4a9d49a3572c8b8ac4649ab62
Lester <lestermt94@gmail.com>
2016-10-26 22:23:42 -0600

    Actualiza

--- public/js/lineainvestigacion.js
+++ public/js/lineainvestigacion.js
@@ -52,3 +52,71 @@ $('#agregar').on('click', function(){
 		}
 	});
 });
+
+
+
+
+function danger(btn){
+	$('#confirmRemove').val(btn.value);
+}
+
+function eliminar(btn){
+	$.ajax({
+		url: '/lineainvestigacion/'+btn.value,
+		headers: {'X-CSRF-TOKEN': $('#token').val()},
+		type: 'DELETE',
+		dataType: 'json',
+		success: function(){
+			listar();
+			$('#modalRemove').modal('toggle');
+			$('#msjuser').removeClass('alert-danger');
+			$('#msjuser').addClass('alert-success');
+			$('#msjuser'+'-text').html('Registros actualizados exitosamente!');
+			$('#msjuser').fadeIn();
+			window.setTimeout(function(){$('#msjuser').fadeOut();}, 2000);
+		}
+	});
+}
+
+function mostrar(btn){
+	$.get('/lineainvestigacion/'+btn.value+'/edit', function(res){
+		$('#nombreliA').val(res.nombrelineainvestigacion);
+	})	
+}
+
+$('#actualizar').on('click', function(){
+	var JData;
+			JData = {
+				nombrelineainvestigacion: $('#nombreliA').val()
+			};
+		
+	
+	$.ajax({
+		url: '/lineainvestigacion/'+$('#idn').val(),
+		headers: {'X-CSRF-TOKEN': $('#token').val()},
+		type: 'PUT',
+		dataType: 'json',
+		data: JData,
+		success: function(){
+			listar();
+			$('#modalEdit').modal('toggle');
+			$('#nombreliA').val('');
+			
+			
+		
+			$('#msjuser').removeClass('alert-danger');
+			$('#msjuser').addClass('alert-success');
+			$('#msjuser'+'-text').html('Registros actualizados exitosamente!');
+			$('#msjuser').fadeIn();
+			window.setTimeout(function(){$('#msjuser').fadeOut();}, 2000);
+		},
+		error:function(msj){
+			$('#msjuser').removeClass('alert-success');
+			$('#msjuserA').addClass('alert-danger');
+			$('#msjuserA'+'-text').html((msj.responseJSON.nombrelineainvestigacion!=undefined)?msj.responseJSON.nombrelineainvestigacion:'');
+			$('#msjuserA').fadeIn();
+			window.setTimeout(function(){$('#msjuserA').fadeOut();}, 2000);
+		}
+	});
+})
+
