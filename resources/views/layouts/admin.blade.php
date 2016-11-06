3edaad89d13a3b62d442aa9ad24138fb43d1a51b
Lester Mendoza <lestermt94@gmail.com>
2016-11-05 19:57:51 -0600

    fa fa-unlock

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -81,7 +81,7 @@
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a id="userPassButton"><i class="fa fa-user pull-right"></i>Cambiar Contraseña</a></li>
+                    <li><a id="userPassButton"><i class="fa fa-lock pull-right"></i>Cambiar Contraseña</a></li>
                     
                     <li><a href="{!!URL::to('/logout')!!}"><i class="fa fa-sign-out pull-right"></i>Cerrar Sesión</a></li>
                   </ul>
