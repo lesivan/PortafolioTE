05d7fcfee11f0263715c861287eccb376d1d2596
Lester <lestermt94@gmail.com>
2016-11-09 19:59:31 -0600

    rutas agregadas

--- resources/views/estudiante/edit.blade.php
+++ resources/views/estudiante/edit.blade.php
@@ -4,17 +4,16 @@
 	@include('alerts.request')
 	<div id="divider"></div>
 	<div class="form-group">
-		{!!Form::model($user, ['route'=>['estudiante.update', $carrera->id], 'method'=>'PUT'])!!}
+		{!!Form::model($estudiante, ['route'=>['estudiante.update', $estudiante->id], 'method'=>'PUT'])!!}
 			@include('estudiante.forms.usr')
 			{!!Form::submit('Modificar', ['class'=>'btn btn-primary'])!!}
 		{!!Form::close()!!}
 	</div>
-	@if ($user->id > 5)
+
 	<div class="form-group">
-		{!!Form::open(['route'=>['estudiante.destroy', $carrera->id], 'method'=>'DELETE'])!!}
+		{!!Form::open(['route'=>['estudiante.destroy', $estudiante->id], 'method'=>'DELETE'])!!}
 			{!!Form::submit('Eliminar', ['class'=>'btn btn-danger'])!!}
 		{!!Form::close()!!}
 	</div>
-	@endif
 	
 @stop
