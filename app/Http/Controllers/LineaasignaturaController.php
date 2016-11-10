461d008a415b46197a1a5b3e3137d1e747cbb38c
Lester <lestermt94@gmail.com>
2016-11-09 20:01:34 -0600

    Lineas y asignaturas

---
+++ app/Http/Controllers/LineaasignaturaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace PortafolioTE\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use PortafolioTE\Http\Requests;
+use PortafolioTE\Http\Requests\LineaasignaturaRequest;
+
+use PortafolioTE\LInvestAsignatura;
+use DB;
+use Carbon\Carbon;
+
+class LineaasignaturaController extends Controller
+{
+    public function index(){
+		return view('lineaasignatura.index');
+	}
+
+	public function create(){
+		return view('lineaasignatura.create');
+	}
+
+	public function typesLineas(){
+		$typesL = DB::select('SELECT * FROM lineainvestigacion');
+		return response()->json(
+            $typesL
+        );
+	}
+
+	public function typesAsignaturas(){
+		$typesA = DB::select('SELECT * FROM asignatura');
+		return response()->json(
+            $typesA
+        );
+	}
+
+	public function listing(){
+		$lineaasignatura = DB::select("SELECT linvestasignatura.id, linvestasignatura.id, asignatura.nombreasignatura, linvestasignatura.id, lineainvestigacion.nombrelineainvestigacion FROM linvestasignatura inner join asignatura on linvestasignatura.idasig = asignatura.id inner join lineainvestigacion on linvestasignatura.idlineainvestigacion = lineainvestigacion.id");
+		return response()->json(
+            $lineaasignatura
+        );
+	}
+
+	public function store(LineaasignaturaRequest $req){
+    	if($req->ajax() ){
+    		LInvestAsignatura::create($req->all());
+    		return response()->json([
+    			'mensaje' => "creado"
+    		]);
+    	}
+    }
+}
